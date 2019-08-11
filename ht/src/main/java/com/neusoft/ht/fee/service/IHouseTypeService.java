e3837b34ac2a880361dede32b82d6e3f2010dfa2
negosan <425569885@qq.com>
2019-08-11 15:55:14 +0800

    完成供热缴费模块的房屋类型管理&付款类型管理的后端代码并通过测试 by 黄宇德

---
+++ ht/src/main/java/com/neusoft/ht/fee/service/IHouseTypeService.java
@@ -0,0 +1,28 @@
+package com.neusoft.ht.fee.service;
+
+import java.util.List;
+
+import com.neusoft.ht.fee.model.HouseTypeModel;
+
+public interface IHouseTypeService {
+
+	//增加
+	public void add(HouseTypeModel houseTypeModel) throws Exception;
+	//修改
+	public void modify(HouseTypeModel hosHouseTypeModel) throws Exception;
+	//删除
+	public void delete(HouseTypeModel houseTypeModel) throws Exception;
+	//取得单个对象
+	public HouseTypeModel getByNo(int typeno) throws Exception;
+	//取得所有对象
+	public List<HouseTypeModel> getListByAll() throws Exception;
+
+	//取得所有对象列表,分页模式
+	public List<HouseTypeModel> getListByAllWithPage(int rows,int page) throws Exception;
+	//取得对象的个数
+	public int getCountByAll() throws Exception;
+	//取得对象页数
+	public int getPagaCountByAll(int rows) throws Exception;
+
+
+}
---
+++ ht/src/main/java/com/neusoft/ht/fee/service/IPaymentTypeService.java
@@ -0,0 +1,26 @@
+package com.neusoft.ht.fee.service;
+
+import java.util.List;
+
+import com.neusoft.ht.fee.model.PaymentTypeModel;
+
+public interface IPaymentTypeService {
+	//增加
+	public void add(PaymentTypeModel paymentTypeModel ) throws Exception;
+	//修改
+	public void modify(PaymentTypeModel paymentTypeModel) throws Exception;
+	//删除
+	public void delete(PaymentTypeModel paymentTypeModel) throws Exception;
+	//取得单个对象
+	public PaymentTypeModel getByNo(int typeno) throws Exception;
+	//取得所有对象
+	public List<PaymentTypeModel> getListByAll() throws Exception;
+
+	//取得所有对象列表,分页模式
+	public List<PaymentTypeModel> getListByAllWithPage(int rows,int page) throws Exception;
+	//取得对象的个数
+	public int getCountByAll() throws Exception;
+	//取得对象页数
+	public int getPagaCountByAll(int rows) throws Exception;
+
+}
